761efee498fbb771bb4759af06fde548e7419101
HighDoping <highdoping@gmail.com>
2025-02-28 00:29:49 +0800

    Optimization for Mac.

--- wan/text2video.py
+++ wan/text2video.py
@@ -18,13 +18,23 @@ from .distributed.fsdp import shard_model
 from .modules.model import WanModel
 from .modules.t5 import T5EncoderModel
 from .modules.vae import WanVAE
-from .utils.fm_solvers import (FlowDPMSolverMultistepScheduler,
-                               get_sampling_sigmas, retrieve_timesteps)
+from .utils.fm_solvers import (
+    FlowDPMSolverMultistepScheduler,
+    get_sampling_sigmas,
+    retrieve_timesteps,
+)
 from .utils.fm_solvers_unipc import FlowUniPCMultistepScheduler
 
 
-class WanT2V:
+def clear_cache():
+    gc.collect()
+    if torch.backends.mps.is_built():
+        torch.mps.empty_cache()
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
 
+
+class WanT2V:
     def __init__(
         self,
         config,
@@ -57,71 +67,72 @@ class WanT2V:
             t5_cpu (`bool`, *optional*, defaults to False):
                 Whether to place T5 model on CPU. Only works without t5_fsdp.
         """
-        self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+        self.device = torch.device(
+            "mps" if torch.backends.mps.is_available() else "cpu"
+        )
         self.config = config
+        self.checkpoint_dir = checkpoint_dir
+        self.device_id = device_id
         self.rank = rank
         self.t5_cpu = t5_cpu
+        self.t5_fsdp = t5_fsdp
 
         self.num_train_timesteps = config.num_train_timesteps
         self.param_dtype = config.param_dtype
 
-        shard_fn = partial(shard_model, device_id=device_id)
-        self.text_encoder = T5EncoderModel(
-            text_len=config.text_len,
-            dtype=config.t5_dtype,
-            device=torch.device('cpu'),
-            checkpoint_path=os.path.join(checkpoint_dir, config.t5_checkpoint),
-            tokenizer_path=os.path.join(checkpoint_dir, config.t5_tokenizer),
-            shard_fn=shard_fn if t5_fsdp else None)
+        # self.shard_fn = partial(shard_model, device_id=self.device_id)
 
         self.vae_stride = config.vae_stride
         self.patch_size = config.patch_size
-        self.vae = WanVAE(
-            vae_pth=os.path.join(checkpoint_dir, config.vae_checkpoint),
-            device=self.device)
 
         logging.info(f"Creating WanModel from {checkpoint_dir}")
-        self.model = WanModel.from_pretrained(checkpoint_dir)
-        self.model.eval().requires_grad_(False)
 
-        if use_usp:
-            try:
-                from xfuser.core.distributed import get_sequence_parallel_world_size
-                from .distributed.xdit_context_parallel import (usp_attn_forward,
-                                                                usp_dit_forward)
-                for block in self.model.blocks:
-                    block.self_attn.forward = types.MethodType(
-                        usp_attn_forward, block.self_attn)
-                self.model.forward = types.MethodType(usp_dit_forward, self.model)
-                self.sp_size = get_sequence_parallel_world_size()
-            except ImportError:
-                # Fall back to non-USP mode on systems without xfuser
-                logging.warning("xfuser package not found, disabling USP mode")
-                self.sp_size = 1
-                use_usp = False
-        else:
-            self.sp_size = 1
+        # if use_usp:
+        #     try:
+        #         from xfuser.core.distributed import get_sequence_parallel_world_size
 
-        if dist.is_initialized():
-            dist.barrier()
-        if dit_fsdp:
-            self.model = shard_fn(self.model)
-        else:
-            self.model.to(self.device)
+        #         from .distributed.xdit_context_parallel import (
+        #             usp_attn_forward,
+        #             usp_dit_forward,
+        #         )
+
+        #         for block in self.model.blocks:
+        #             block.self_attn.forward = types.MethodType(
+        #                 usp_attn_forward, block.self_attn
+        #             )
+        #         self.model.forward = types.MethodType(usp_dit_forward, self.model)
+        #         self.sp_size = get_sequence_parallel_world_size()
+        #     except ImportError:
+        #         # Fall back to non-USP mode on systems without xfuser
+        #         logging.warning("xfuser package not found, disabling USP mode")
+        #         self.sp_size = 1
+        #         use_usp = False
+        # else:
+        #     self.sp_size = 1
+        self.sp_size = 1
+
+        # if dist.is_initialized():
+        #     dist.barrier()
+        # if dit_fsdp:
+        #     self.model = self.shard_fn(self.model)
+        # else:
+        #     self.model.to(self.device)
 
         self.sample_neg_prompt = config.sample_neg_prompt
 
-    def generate(self,
-                 input_prompt,
-                 size=(1280, 720),
-                 frame_num=81,
-                 shift=5.0,
-                 sample_solver='unipc',
-                 sampling_steps=50,
-                 guide_scale=5.0,
-                 n_prompt="",
-                 seed=-1,
-                 offload_model=True):
+    def generate(
+        self,
+        input_prompt,
+        size=(1280, 720),
+        frame_num=81,
+        shift=5.0,
+        sample_solver="unipc",
+        sampling_steps=50,
+        guide_scale=5.0,
+        n_prompt="",
+        seed=-1,
+        offload_model=True,
+    ):
         r"""
         Generates video frames from text prompt using diffusion process.
 
@@ -157,13 +168,23 @@ class WanT2V:
         """
         # preprocess
         F = frame_num
-        target_shape = (self.vae.model.z_dim, (F - 1) // self.vae_stride[0] + 1,
-                        size[1] // self.vae_stride[1],
-                        size[0] // self.vae_stride[2])
+        vae_model_z_dim = 16
+        target_shape = (
+            vae_model_z_dim,
+            (F - 1) // self.vae_stride[0] + 1,
+            size[1] // self.vae_stride[1],
+            size[0] // self.vae_stride[2],
+        )
 
-        seq_len = math.ceil((target_shape[2] * target_shape[3]) /
-                            (self.patch_size[1] * self.patch_size[2]) *
-                            target_shape[1] / self.sp_size) * self.sp_size
+        seq_len = (
+            math.ceil(
+                (target_shape[2] * target_shape[3])
+                / (self.patch_size[1] * self.patch_size[2])
+                * target_shape[1]
+                / self.sp_size
+            )
+            * self.sp_size
+        )
 
         if n_prompt == "":
             n_prompt = self.sample_neg_prompt
@@ -171,17 +192,34 @@ class WanT2V:
         seed_g = torch.Generator(device=self.device)
         seed_g.manual_seed(seed)
 
+        self.text_encoder = T5EncoderModel(
+            text_len=self.config.text_len,
+            dtype=self.config.t5_dtype,
+            device=torch.device("cpu"),
+            checkpoint_path=os.path.join(
+                self.checkpoint_dir, self.config.t5_checkpoint
+            ),
+            tokenizer_path=os.path.join(self.checkpoint_dir, self.config.t5_tokenizer),
+            # shard_fn=self.shard_fn if self.t5_fsdp else None,
+            shard_fn=None,
+        )
+
         if not self.t5_cpu:
             self.text_encoder.model.to(self.device)
             context = self.text_encoder([input_prompt], self.device)
             context_null = self.text_encoder([n_prompt], self.device)
-            if offload_model:
-                self.text_encoder.model.cpu()
         else:
-            context = self.text_encoder([input_prompt], torch.device('cpu'))
-            context_null = self.text_encoder([n_prompt], torch.device('cpu'))
+            context = self.text_encoder([input_prompt], torch.device("cpu"))
+            context_null = self.text_encoder([n_prompt], torch.device("cpu"))
             context = [t.to(self.device) for t in context]
             context_null = [t.to(self.device) for t in context_null]
+        if offload_model:
+            del self.text_encoder
+            clear_cache()
+
+        self.model = WanModel.from_pretrained(self.checkpoint_dir)
+        self.model.eval().requires_grad_(False)
+        self.model.to(self.device)
 
         noise = [
             torch.randn(
@@ -191,44 +229,46 @@ class WanT2V:
                 target_shape[3],
                 dtype=torch.float32,
                 device=self.device,
-                generator=seed_g)
+                generator=seed_g,
+            )
         ]
 
         @contextmanager
         def noop_no_sync():
             yield
 
-        no_sync = getattr(self.model, 'no_sync', noop_no_sync)
+        no_sync = getattr(self.model, "no_sync", noop_no_sync)
 
         # evaluation mode
         with amp.autocast(dtype=self.param_dtype), torch.no_grad(), no_sync():
-
-            if sample_solver == 'unipc':
+            if sample_solver == "unipc":
                 sample_scheduler = FlowUniPCMultistepScheduler(
                     num_train_timesteps=self.num_train_timesteps,
                     shift=1,
-                    use_dynamic_shifting=False)
+                    use_dynamic_shifting=False,
+                )
                 sample_scheduler.set_timesteps(
-                    sampling_steps, device=self.device, shift=shift)
+                    sampling_steps, device=self.device, shift=shift
+                )
                 timesteps = sample_scheduler.timesteps
-            elif sample_solver == 'dpm++':
+            elif sample_solver == "dpm++":
                 sample_scheduler = FlowDPMSolverMultistepScheduler(
                     num_train_timesteps=self.num_train_timesteps,
                     shift=1,
-                    use_dynamic_shifting=False)
+                    use_dynamic_shifting=False,
+                )
                 sampling_sigmas = get_sampling_sigmas(sampling_steps, shift)
                 timesteps, _ = retrieve_timesteps(
-                    sample_scheduler,
-                    device=self.device,
-                    sigmas=sampling_sigmas)
+                    sample_scheduler, device=self.device, sigmas=sampling_sigmas
+                )
             else:
                 raise NotImplementedError("Unsupported solver.")
 
             # sample videos
             latents = noise
 
-            arg_c = {'context': context, 'seq_len': seq_len}
-            arg_null = {'context': context_null, 'seq_len': seq_len}
+            arg_c = {"context": context, "seq_len": seq_len}
+            arg_null = {"context": context_null, "seq_len": seq_len}
 
             for _, t in enumerate(tqdm(timesteps)):
                 latent_model_input = latents
@@ -237,33 +277,42 @@ class WanT2V:
                 timestep = torch.stack(timestep)
 
                 self.model.to(self.device)
-                noise_pred_cond = self.model(
-                    latent_model_input, t=timestep, **arg_c)[0]
+                noise_pred_cond = self.model(latent_model_input, t=timestep, **arg_c)[0]
                 noise_pred_uncond = self.model(
-                    latent_model_input, t=timestep, **arg_null)[0]
+                    latent_model_input, t=timestep, **arg_null
+                )[0]
 
                 noise_pred = noise_pred_uncond + guide_scale * (
-                    noise_pred_cond - noise_pred_uncond)
+                    noise_pred_cond - noise_pred_uncond
+                )
 
                 temp_x0 = sample_scheduler.step(
                     noise_pred.unsqueeze(0),
                     t,
                     latents[0].unsqueeze(0),
                     return_dict=False,
-                    generator=seed_g)[0]
+                    generator=seed_g,
+                )[0]
                 latents = [temp_x0.squeeze(0)]
 
             x0 = latents
             if offload_model:
-                self.model.cpu()
-                torch.cuda.empty_cache()
+                # self.model.cpu()
+                del self.model
+                clear_cache()
             if self.rank == 0:
+                self.vae = WanVAE(
+                    vae_pth=os.path.join(
+                        self.checkpoint_dir, self.config.vae_checkpoint
+                    ),
+                    device=self.device,
+                )
                 videos = self.vae.decode(x0)
 
         del noise, latents
         del sample_scheduler
         if offload_model:
-            gc.collect()
+            clear_cache()
             if torch.cuda.is_available():
                 torch.cuda.synchronize()
         if dist.is_initialized():
